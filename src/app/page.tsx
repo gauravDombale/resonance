1875c6328ddd463c12ef909c7166f61499c7a31e
gauravDombale <gauravdombale007@gmail.com>
2026-03-21 15:35:15 +0530

    ui changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
 
 export default function Home() {
   return (
-   <Button variant="outline">Hello World</Button>
+    <Button
+      onClick={() => {
+        toast.success("Hello");
+      }}
+    >
+      Hello World
+    </Button>
   );
 }
